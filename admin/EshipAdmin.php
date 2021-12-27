b744f1bdc4911945a6b03ef59aa0f7cdc35148bc
Juan Manuel <jmlo201119@outlook.com>
2021-12-27 14:50:06 -0600

    Estructura simple

--- admin/EshipAdmin.php
+++ admin/EshipAdmin.php
@@ -1,6 +1,8 @@
 <?php
+namespace Eship\Admin;
 
-class EshipAdmin
-{
-
+class EshipAdmin {
+    public function __construct(){
+        echo __CLASS__;
+    }
 }
--- admin/load_admin.php
+++ admin/load_admin.php
@@ -1,1 +1,8 @@
 <?php
+require_once __DIR__ . "/EshipAdmin.php";
+use Eship\Admin\EshipAdmin as Admin;
+
+if (! class_exists('EshipAdmin')) {
+    $adm = new \Eship\Admin\EshipAdmin();
+    var_dump($adm);
+}
