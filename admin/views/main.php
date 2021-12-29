cdd5953e372e71220aa4315b05be90cc3f38ffe9
Juan Manuel <jmlo201119@outlook.com>
2021-12-28 18:37:18 -0600

    se arregla vista

--- admin/views/main.php
+++ admin/views/main.php
@@ -1,12 +1,14 @@
 <?php
 function eShip_menu_page() {
+    $adm = new Eship\Admin\Eship_Admin();
+    //var_dump($adm);
     add_menu_page(
         'Eship',
         'Eship',
         'manage_options',
         'Eship',
         'eShip_menu_page_display',
-        dirname(dirname(plugin_dir_path( __FILE__ ))) . "/public/image/icon/icon.favicon",
+        '',//$adm->getIconMenu(),
         30
     );
 
@@ -56,19 +58,39 @@ function add_submenus_page($submenus)
 }
 
 function eShip_menu_page_display(){
+    $adm = new Eship\Admin\Eship_Admin();
+    $menu_header = $adm->getHeaderImg();
+    $wc_img = $adm->getImgWoocommerce();
+    require_once dirname(__FILE__) . "/partials/navbar.php";
     require_once dirname(__FILE__) . "/dashboard.php";
+    require_once dirname(__FILE__) . "/partials/footer.php";
 }
 
 function eShip_submenu_configuration_page() {
+    $adm = new Eship\Admin\Eship_Admin();
+    $menu_header = $adm->getHeaderImg();
+    $wc_img = $adm->getImgWoocommerce();
+    require_once dirname(__FILE__) . "/partials/navbar.php";
     require_once dirname(__FILE__) . "/configuration.php";
+    require_once dirname(__FILE__) . "/partials/footer.php";
 }
 
 function eShip_submenu_tracking_guide_page() {
+    $adm = new Eship\Admin\Eship_Admin();
+    $menu_header = $adm->getHeaderImg();
+    $adm->enqueue_scripts('tracking_guide_js', 'admin/views/partials/tracking_guide/tracking-guide.js', '0.1a');
+    require_once dirname(__FILE__) . "/partials/navbar.php";
     require_once dirname(__FILE__) . "/tracking_guide.php";
+    require_once dirname(__FILE__) . "/partials/footer.php";
 }
 
 function eShip_submenu_quote_page() {
+    $adm = new Eship\Admin\Eship_Admin();
+    $menu_header = $adm->getHeaderImg();
+    $adm->enqueue_scripts('quote_js', 'admin/views/partials/quote/quote.js', '0.1a');
+    require_once dirname(__FILE__) . "/partials/navbar.php";
     require_once dirname(__FILE__) . "/quote.php";
+    require_once dirname(__FILE__) . "/partials/footer.php";
 }
 
 add_action( 'admin_menu', 'eShip_menu_page' );
