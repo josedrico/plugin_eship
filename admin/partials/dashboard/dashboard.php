4222184a224d7d598cbc5bb4248b1cfa70a42efd
Juan Manuel <jmlo201119@outlook.com>
2022-01-06 17:52:10 -0600

    Vista de ordenes

--- admin/partials/dashboard/dashboard.php
+++ admin/partials/dashboard/dashboard.php
@@ -1,3 +1,4 @@
+<!--
 <div class="container">
     <div class="mt-5 mb-5">
         <h5 class="mb-2 ms-1">Connections with e-commerce platform.</h5>
@@ -13,6 +14,32 @@
                         officiis placeat porro quasi rem repellat ut veniam vero voluptatem.
                         <div id="chart-one" style="width: 100%;height:800px;"></div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+-->
+
+<div class="container">
+    <div class="mt-5 mb-5">
+        <h5>Ordenes de Woocommerce.</h5>
+        <div class="row">
+            <div class="col-12">
+                <div class="col-12">
+                    <table id="orders" class="table">
+                        <thead>
+                        <tr>
+                            <th data-field="orderDate">Order / Date</th>
+                            <th data-field="client">Client</th>
+                            <th data-field="items">Items</th>
+                            <th data-field="shipment">Shipment</th>
+                            <th data-field="paid">Paid</th>
+                            <th data-field="fulfilled">Fulfilled</th>
+                            <th data-field="label">Label</th>
+                        </tr>
+                        </thead>
+                    </table>
                 </div>
             </div>
         </div>
