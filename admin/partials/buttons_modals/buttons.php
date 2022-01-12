1c8b61636d0a5b8790e7de4c9130cee169748e75
Juan Manuel <jmlo201119@outlook.com>
2022-01-11 22:24:41 -0600

    Vista de buttons

--- admin/partials/buttons_modals/buttons.php
+++ admin/partials/buttons_modals/buttons.php
@@ -1,1 +1,9 @@
-<?php
+<div class="app-eship">
+    <div class="container">
+        <!-- Button trigger modal -->
+        <a class="btn btn-link" data-bs-toggle="modal" href="#dashBoardEshipModalToggle" role="button">
+            Ship Now <i class="fas fa-shipping-fast"></i>
+        </a>
+        <?php (isset($modal_custom))? require_once $modal_custom : FALSE;?>
+    </div>
+</div>
