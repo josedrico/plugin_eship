9e6d6553f8512b75047c6192f39e37c6fc128c9d
Juan Manuel <jmlo201119@outlook.com>
2022-01-14 15:52:05 -0600

    Shipment

---
+++ admin/class-eship-shipment.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace EshipAdmin;
+require_once ESHIP_PLUGIN_DIR_PATH . 'admin/class-eship-woocommerce-api.php';
+require_once ESHIP_PLUGIN_DIR_PATH . 'admin/class-eship-api.php';
+
+use EshipAdmin\ESHIP_Api;
+use EshipAdmin\ESHIP_Woocommerce_Api;
+
+/**
+ * Config and queries to api's.
+ *
+ * @link       https://eship.com
+ * @since      1.0.0
+ *
+ * @package    ESHIP_blank
+ * @subpackage ESHIP_blank/admin
+ */
+class ESHIP_Shipment {
+    private $rate_id;
+    //private $woocommerce_api;
+    private $eship_api;
+
+    public function __construct($rate_id)
+    {
+        $this->rate_id = $rate_id;
+        //$this->woocommerce_api  = new ESHIP_Woocommerce_Api();
+        $this->eship_api        = new ESHIP_Api();
+    }
+
+    public function getShipment()
+    {
+        $json       = json_encode(array(
+            'rate'  => $this->rate_id
+        ));
+        $response   = wp_remote_retrieve_body( $this->eship_api->post('shipment', $json) );
+        //echo json_decode($response);
+        wp_send_json($response);
+    }
+}
