a934735d170742dea9b27f6b0195c1cd120faa39
Juan Manuel <jmlo201119@outlook.com>
2022-01-13 19:06:50 -0600

    api eship

--- admin/class-eship-api.php
+++ admin/class-eship-api.php
@@ -2,5 +2,51 @@
 namespace EshipAdmin;
 
 class ESHIP_Api {
+    private $url;
+    private $api_key;
 
+    public function __construct()
+    {
+        $this->setUrl();
+        $this->setApiKey();
+    }
+
+    private function setUrl()
+    {
+        $this->url = "https://api.myeship.co/rest/";
+    }
+
+    private function setApiKey()
+    {
+        $this->api_key = 'eship_prod_835261c341f8465b2';
+    }
+
+    public function post($uri, $body)
+    {
+        return wp_remote_post(
+            $this->url . $uri,
+            array(
+                'method'      => 'POST',
+                //'timeout'     => 45,
+                //'redirection' => 5,
+                //'httpversion' => '1.0',
+                'blocking'    => true,
+                'headers'     => array(
+                    'content-Type'  => 'Application/json',
+                    'api-key'       => $this->api_key
+                ),
+                'body'      => $body,
+                'cookies'   => array()
+            )
+        );
+    }
+
+    public function get($uri) {
+        return wp_remote_get($this->url . $uri, array(
+            'headers' => array(
+                'content-Type'  => 'Application/json',
+                'api-key'       => $this->api_key
+            )
+        ));
+    }
 }
