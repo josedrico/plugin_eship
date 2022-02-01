ab32b1fac62d1da8d95ebead75e0eb031799cfe0
Juan Manuel <jmlo201119@outlook.com>
2022-01-31 18:38:57 -0600

    Vista de shipment

--- admin/partials/buttons_modals/shipment_sheet.php
+++ admin/partials/buttons_modals/shipment_sheet.php
@@ -1,1 +1,26 @@
-<?php
+<!-- Modal -->
+<div class="modal fade" id="shipmentPdfModal" tabindex="-1" aria-labelledby="shipmentPdfModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-xl modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="shipmentModalToggleLabel2">
+                        Tracking number: <a href="https://app.myeship.co/en/track/track?no=<?php echo (isset($pdf_arr['tracking_number']))? $pdf_arr['tracking_number'] : '';?>" target="_blank"><?php echo (isset($pdf_arr['tracking_number']))? $pdf_arr['tracking_number'] : '';?></a>
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div>
+                    <div class="ratio ratio-16x9">
+                        <iframe src="<?php echo (isset($pdf_arr['tracking_link']))? $pdf_arr['tracking_link'] : '';?>" title="<?php echo (isset($pdf_arr['provider']))? $pdf_arr['provider'] : '';?>" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <!--
+                <button class="btn btn-secondary" data-bs-target="#dashBoardEshipModalToggle" data-bs-toggle="modal">Back</button>
+                -->
+            </div>
+        </div>
+    </div>
+</div>
