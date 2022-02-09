baeff510562c168622b1d408d1894c56e4b092c5
Juan Manuel <jmlo201119@outlook.com>
2022-02-09 01:45:27 -0600

    nueva tabla

--- admin/partials/connection/dashboard_connection.php
+++ admin/partials/connection/dashboard_connection.php
@@ -88,6 +88,13 @@
                                             <input type="email" class="form-control" id="email-input-eship" name="emailCompanyEship" <?php echo (isset($user_eship[0]->email))? "value='".$user_eship[0]->email."'": '';?>>
                                         </div>
                                     </div>
+                                    <div class="col-12">
+                                        <h4 class="mt-1">Config Dimensions & Weigth</h4>
+                                        <label for="activate-config-input-eship" class="col-form-label">
+                                            <input class="form-check-input" type="checkbox" id="activate-config-input-eship" value="1" aria-label="Activate" name="activateConfigEship" <?php echo (isset($user_eship[0]->dimensions) && $user_eship[0]->dimensions == 1)? 'checked': '';?>>
+                                            Activate dimension and weigth to every product.
+                                        </label>
+                                    </div>
                                     <div class="row g-1 mb-4">
                                         <div class="col-12 col-md-6 offset-md-6">
                                             <button type="submit" class="btn btn-primary w-100" id="<?php echo $config_data['btn'];?>">
@@ -104,90 +111,140 @@
                     </div>
                     <div class="tab-pane fade" id="nav-package" role="tabpanel" aria-labelledby="nav-package-tab">
                         <div class="row mt-3">
-                            <div class="col-12">
-                                <h3 class="text-center">
-                                    Package Information
-                                </h3>
+                            <div class="col-12 text-end">
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#eshipDimWeModal">
+                                    Create dimensions
+                                </button>
                             </div>
-                            <div class="row">
-                                <div class="col-12 border border-dark border-start-0 border-end-0 ">
-                                    <h4 class="ps-5 mb-1 mt-1">Custom</h4>
-                                </div>
-                                <div class="col-sm-10 offset-sm-1">
-                                    <form method="post" action="" class="row">
-                                        <div class="col-12">
-                                            <h5>Dimensions</h5>
-                                        </div>
-                                        <div class="col-12 col-md-6">
-                                            <div class="mb-2">
-                                                <label for="length-input-eship" class="col-form-label">
-                                                    Length:
-                                                </label>
-                                                <input type="text" class="form-control" id="length-input-eship" name="lengthEship">
-                                            </div>
-                                        </div>
-                                        <div class="col-12 col-md-6">
-                                            <div class="mb-2">
-                                                <label for="width-input-eship" class="col-form-label">
-                                                    Width:
-                                                </label>
-                                                <input type="text" class="form-control" id="cs-input-eship" name="widthEship">
-                                            </div>
-                                        </div>
-                                        <div class="col-12 col-md-6">
-                                            <div class="mb-2">
-                                                <label for="height-input-eship" class="col-form-label">
-                                                    Height:
-                                                </label>
-                                                <input type="password" class="form-control" id="height-input-eship" name="heightEship">
-                                            </div>
-                                        </div>
-                                        <div class="col-12 col-md-6">
-                                            <div class="mb-2">
-                                                <label for="unit-input-eship" class="col-form-label">
-                                                    Unit:
-                                                </label>
-                                                <select class="form-select" name="unitDimensionsEship" id="unit-input-eship">
-                                                    <option value="cm" selected>cm</option>
-                                                    <option value="in">in</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col-12">
-                                            <h5>Weight</h5>
-                                        </div>
-                                        <div class="col-12 col-md-6">
-                                            <div class="mb-2">
-                                                <label for="weight-input-eship" class="col-form-label">
-                                                    Weight:
-                                                </label>
-                                                <input type="text" class="form-control" id="weigth-input-eship" name="weightEship">
-                                            </div>
-                                        </div>
-                                        <div class="col-12 col-md-6">
-                                            <div class="mb-2">
-                                                <label for="unitWeigth-input-eship" class="col-form-label">
-                                                    Unit:
-                                                </label>
-                                                <select class="form-select" name="unitWeigthEship" id="unitWeigth-input-eship">
-                                                    <option value="kg" selected>kg</option>
-                                                    <option value="lb">lb</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="row g-1 mb-4">
-                                            <div class="col-12 col-md-6 offset-md-6">
-                                                <button type="submit" class="btn btn-primary w-100">
-                                                    <span class="dashicons dashicons-saved"></span>
-                                                    <div id="loader-light" class="spinner-border text-light" role="status" style="width: 1.2rem; height: 1.2rem; display: none;">
-                                                        <span class="visually-hidden">Loading...</span>
-                                                    </div>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
+                            <div class="col-12 mt-2">
+                                <table id="eship-dim-weigth" class="table table-bordered table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th colspan="4">Dimensions</th>
+                                        <th colspan="3">Weight</th>
+                                    </tr>
+                                    <tr>
+                                        <th data-field="length_dim">Length</th>
+                                        <th data-field="width_dim">Width</th>
+                                        <th data-field="height_dim">Height</th>
+                                        <th data-field="unit_dim">Unit</th>
+                                        <th data-field="weight_w">weight</th>
+                                        <th data-field="unit_w">Unit</th>
+                                        <th data-field="actions">Actions</th>
+                                    </tr>
+                                    </thead>
+                                </table>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="eshipDimWeModal" tabindex="-1" aria-labelledby="eshipDimWeModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="eshipDimWeModalLabel">
+                        Create Package Information
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-sm-10 offset-sm-1">
+                            <form method="post" action="" class="row" id="eshipDimWeModalForm">
+                                <div class="col-12">
+                                    <div class="mb-2">
+                                        <label for="length-input-eship" class="col-form-label">
+                                            Alias:
+                                        </label>
+                                        <input type="text" class="form-control" id="alias-input-eship" name="aliasEship">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <h5>Dimensions</h5>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="mb-2">
+                                        <label for="length-input-eship" class="col-form-label">
+                                            Length:
+                                        </label>
+                                        <input type="text" class="form-control" id="length-input-eship" name="lengthEship">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="mb-2">
+                                        <label for="width-input-eship" class="col-form-label">
+                                            Width:
+                                        </label>
+                                        <input type="text" class="form-control" id="width-input-eship" name="widthEship">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="mb-2">
+                                        <label for="height-input-eship" class="col-form-label">
+                                            Height:
+                                        </label>
+                                        <input type="text" class="form-control" id="height-input-eship" name="heightEship">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="mb-2">
+                                        <label for="unit-input-eship" class="col-form-label">
+                                            Unit:
+                                        </label>
+                                        <select class="form-select" name="unitDimensionsEship" id="unit-input-eship">
+                                            <option value="cm" selected>cm</option>
+                                            <option value="in">in</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <h5>Weight</h5>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="mb-2">
+                                        <label for="weight-input-eship" class="col-form-label">
+                                            Weight:
+                                        </label>
+                                        <input type="text" class="form-control" id="weigth-input-eship" name="weightEship">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="mb-2">
+                                        <label for="unitWeigth-input-eship" class="col-form-label">
+                                            Unit:
+                                        </label>
+                                        <select class="form-select" name="unitWeigthEship" id="unitWeigth-input-eship">
+                                            <option value="kg" selected>kg</option>
+                                            <option value="lb">lb</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="mb-2">
+                                        <label for="status-input-eship" class="col-form-label">
+                                            <input class="form-check-input" type="checkbox" id="status-input-eship" value="1" aria-label="Activate" name="statusEship">
+                                            Activate:
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row g-1 mb-4">
+                                    <div class="col-12 col-md-6">
+                                        <button type="button" class="btn btn-secondary w-100" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                    <div class="col-12 col-md-6">
+                                        <button type="submit" class="btn btn-primary w-100" id="eshipDimWeModalBtn">
+                                            <span class="dashicons dashicons-saved"></span>
+                                            <div id="loader-light" class="spinner-border text-light" role="status" style="width: 1.2rem; height: 1.2rem; display: none;">
+                                                <span class="visually-hidden">Loading...</span>
+                                            </div>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
--- includes/class-eship-activator.php
+++ includes/class-eship-activator.php
@@ -32,12 +32,27 @@ class ESHIP_Activator {
             consumer_secret varchar(255) NOT NULL,
             phone varchar(20) NOT NULL,
             name varchar(255) NOT NULL,
-            created_at TIMESTAMP NULL,
-            updated_at TIMESTAMP NULL,
+            dimensions smallint(1) NOT NULL,
             PRIMARY KEY (id)
         );";
 
         $wpdb->query( $sql );
+
+        $sql_dim = "CREATE TABLE IF NOT EXISTS " . ESHIP_TB_DIM . "(
+            id int(11) NOT NULL AUTO_INCREMENT,
+            length_dim decimal(5,2) NOT NULL,
+            width_dim decimal(5,2) NOT NULL,
+            height_dim decimal(5,2) NOT NULL,
+            unit_dim varchar(50) NOT NULL,
+            weight_w decimal(5,2) NOT NULL,
+            unit_w varchar(50) NOT NULL,
+            status smallint(1) NOT NULL,
+            name varchar(255) NOT NULL,
+            created_at TIMESTAMP NULL,
+            PRIMARY KEY (id)
+        );";
+
+        $wpdb->query( $sql_dim );
         
 	}
 
