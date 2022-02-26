42044fd985c1e38c64a53fb7f809bd12e8fb53d7
Juan Manuel <jmlo201119@outlook.com>
2022-02-25 18:24:45 -0600

    Nuevo botón

--- admin/partials/buttons_modals/buttons.php
+++ admin/partials/buttons_modals/buttons.php
@@ -14,10 +14,21 @@
         <?php (isset($modal_token))? require_once $modal_token : FALSE;?>
         <?php if(isset($modal_shipment_pdf_show)):?>
             <!-- Button trigger modal -->
+            <?php if (isset($pdf_arr['tracking_url']) && !empty($pdf_arr['tracking_url'])):?>
             <div class="row">
                 <div class="col-12">
-                    <button id="shipmentPdfModalBtn" type="button"class="page-title-action w-100 mt-1" data-bs-toggle="modal" data-bs-target="#shipmentPdfModal">
-                        Shipment PDF <i class="fas fa-file-pdf"></i>
+                    <a href="<?php echo $pdf_arr['tracking_url'];?>" target="_blank">
+                    <button type="button" class="page-title-action w-100 mt-1">
+                        Track Shipment <i class="fas fa-file-pdf"></i>
+                    </button>
+                    </a>
+                </div>
+            </div>
+            <?php endif;?>
+            <div class="row">
+                <div class="col-12">
+                    <button id="shipmentPdfModalBtn" type="button" class="page-title-action w-100 mt-1" data-bs-toggle="modal" data-bs-target="#shipmentPdfModal">
+                        View Shipping Label <i class="fas fa-file-pdf"></i>
                     </button>
                 </div>
             </div>
